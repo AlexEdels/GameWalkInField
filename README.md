b7c75848b4854996e19ce5e882979640fa7bdb98
Aleksandr Edelstein <alexkdrus@gmail.com>
2022-06-02 11:29:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Here Example Of Interactiv Trough Keyboard
---
+++ scripts/player.js
@@ -0,0 +1,14 @@
+let player = {
+    x: 0,
+    y: 0,
+
+    /**
+     * Метод задает пользователю новое расположение.
+     * @param {{x: int, y: int}} nextPoint - координаты, куда ставим пользовате-
+     * ля
+     */
+    changePosition (nextPoint) {
+        this.x = nextPoint.x;
+        this.y = nextPoint.y;
+    },
+};
